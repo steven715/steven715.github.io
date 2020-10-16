9960edcc3ceace9c64336e019d8466712eaab5e4
unknown <107153@ski.ad>
2020-10-16 10:17:01 +0800

    Updates

---
+++ precache-manifest.aacf82a84202698fb9594aa31a0232f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f7d6c94259771b1489f729cf22c213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a39857cdc676f77a3a5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb344eed7a12f83cd3be",
+    "url": "/static/js/2.1b2069dc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1b2069dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a39857cdc676f77a3a5",
+    "url": "/static/js/main.5d056928.chunk.js"
+  },
+  {
+    "revision": "25adb8eba63c9e197782",
+    "url": "/static/js/runtime-main.c46e58d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
